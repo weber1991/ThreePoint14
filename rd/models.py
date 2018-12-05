a6d864ba095b25683467a236983752c80ff17bf9
weber19911101 <2217994485@qq.com>
2018-12-05 21:57:44 +0800

    添加了留言类型，留言，名册类型的model代码部分

--- rd/models.py
+++ rd/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-
+# 基础配置的参数
 class BaseSetting(models.Model):
     name = models.CharField(max_length=255, verbose_name="字段说明")
     content = models.TextField(verbose_name="内容", null=True)
@@ -17,6 +17,7 @@ class BaseSetting(models.Model):
         verbose_name_plural = verbose_name
 
 
+# 系统用户，包括普通用户，后台用户，领导用户等。
 class BUser(models.Model):
     name = models.CharField(max_length=255, verbose_name='姓名', null=True, blank=True)
     account = models.CharField(max_length=255, verbose_name='账号', null=True, blank=True)
@@ -34,5 +35,48 @@ class BUser(models.Model):
         verbose_name_plural = verbose_name
 
 
+# 领导名册类型
+class BUserListType(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True, verbose_name='类型名称')
+    type = models.ForeignKey("self", null=True, blank=True, verbose_name="父类名称")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "领导名册类型"
+        verbose_name_plural = verbose_name
+
+
+
+
+class BMessageType(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True, verbose_name='留言类型')
+    # 设计父级类型，可以实现无限深度的类型分类
+    type = models.ForeignKey('self', null=True, blank=True, verbose_name='父级类型')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "留言类型"
+        verbose_name_plural = verbose_name
+
+
 class BMessage(models.Model):
-    title = models.CharField(max_length=255, verbose_name='标题', null=True, blank=True)
+    user = models.ForeignKey(BUser, verbose_name='留言人',null=True, blank=True)
+    title = models.CharField(max_length=255, verbose_name='留言标题', null=True, blank=True)
+    content = models.TextField(verbose_name='留言内容', null=True, blank=True)
+    settime = models.DateTimeField(verbose_name='留言时间', null=True, blank=True)
+    ipaddress = models.CharField(max_length=255, null=True, blank=True, verbose_name='ip地址')
+    type = models.ForeignKey(BMessageType, verbose_name="留言类型")
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "群众留言"
+        verbose_name_plural = verbose_name
+
+
