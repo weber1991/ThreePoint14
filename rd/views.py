3144f8378edf739422b32aa65375e0c8fe9d1ce8
weber19911101 <2217994485@qq.com>
2018-12-06 14:56:40 +0800

    操作失误，所有东西都没了

--- rd/views.py
+++ rd/views.py
@@ -1,19 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import sessions
+from rd.models import *
 
 # Create your views here.
-
 
 
 def index(req):
     return render(req, 'rd/admin/index.html', {})
 
 
-def print(req):
-    return render(req, 'rd/admin/print.html', {})
-
-
 def login(req):
-    pass
+    username = req.session.get("username", None)
+    if req.method == 'GET':
+        # 判断session是否有记录
+        if username is None:
+            return render(req, 'rd/admin/login.html', {})
+        else:
+            try:
+                user = BUser.objects.get(name=username)
+            except:
+                return render(req, 'rd/admin/login.html', {})
+            return redirect('index:index')
+    else:
+        # 对账号密码进行验证
+        username = req.POST.get('username', None)
+        password = req.POST.get('password', None)
+        if username and password:
+            try:
+                user = BUser.objects.filter(name=username).filter(passwrod=password)
+            except:
+                message = "请输入正确的账号和密码。"
+                return render(req, 'rd/admin/loginMessage.html', {'message':message})
+            if user.count() >= 1:
+                req.session["username"] = username
+                return redirect('rd:index')
+            else:
+                message = "请输入正确的账号和密码。"
+                return render(req, 'rd/admin/loginMessage.html', {'message':message})
+        else:
+            message = "请输入正确的账号和密码。"
+            return render(req, 'rd/admin/loginMessage.html', {'message':message})
+
 
 def register(req):
     pass
